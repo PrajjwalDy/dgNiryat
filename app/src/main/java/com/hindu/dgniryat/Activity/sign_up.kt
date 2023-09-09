867e685d6d96b04b9be91a691e6a07fc3e66cfda
Ashutosh <astcy@yahoo.com>
2023-09-09 20:13:13 +0530

    Login

---
+++ app/src/main/java/com/hindu/dgniryat/Activity/sign_up.kt
@@ -0,0 +1,12 @@
+package com.hindu.dgniryat.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hindu.dgniryat.R
+
+class sign_up : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
