f78c173ff36f968b829fb07162c2e62e50233550
PrajjwalDy <21BCS2246@cuchd.in>
2023-09-18 18:50:39 +0530

    home page completed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,8 +57,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation ("com.google.zxing:core:3.4.1")
-    implementation ("com.journeyapps:zxing-android-embedded:3.6.0")
+    implementation ("com.google.zxing:core:3.5.2")
+    implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
 
     implementation("com.razorpay:checkout:1.6.33")
 
