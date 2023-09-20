aa1297f137635cfbeb802ea21aa28cb0dfc753d6
PrajjwalDy <21BCS2246@cuchd.in>
2023-09-21 03:26:54 +0530

    article adding article tracking barcode generation article details

--- app/src/main/java/com/hindu/dgniryat/Fragments/PostageCalculation.kt
+++ app/src/main/java/com/hindu/dgniryat/Fragments/PostageCalculation.kt
@@ -1,17 +1,22 @@
 package com.hindu.dgniryat.Fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.navigation.fragment.findNavController
 import com.hindu.dgniryat.R
 import org.w3c.dom.Text
@@ -20,14 +25,14 @@ class PostageCalculation : Fragment() {
 
     var selectedCountry = ""
 
-    private lateinit var weight:EditText
-    private lateinit var country:TextView
+    private lateinit var weight: EditText
+    private lateinit var country: TextView
 
-    private lateinit var grossWeight:TextView
-    private lateinit var gstAmount:TextView
-    private lateinit var estimatedCost:TextView
+    private lateinit var grossWeight: TextView
+    private lateinit var gstAmount: TextView
+    private lateinit var estimatedCost: TextView
 
-    private lateinit var calculate:AppCompatButton
+    private lateinit var calculate: AppCompatButton
 
 
     override fun onCreateView(
@@ -35,7 +40,7 @@ class PostageCalculation : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val root:View = inflater.inflate(R.layout.fragment_postage_calculation, container, false)
+        val root: View = inflater.inflate(R.layout.fragment_postage_calculation, container, false)
 
 
         val selectCountry = root.findViewById<Spinner>(R.id.selectCountry_cp)
@@ -75,83 +80,92 @@ class PostageCalculation : Fragment() {
 
         calculate.setOnClickListener {
             calculatePostage()
+            hideKeyboard()
+            Toast.makeText(context,"Calculation Completed",Toast.LENGTH_SHORT).show()
         }
 
         return root
     }
 
-    private fun calculatePostage(){
+    private fun hideKeyboard() {
+        val imm =
+            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm?.hideSoftInputFromWindow(view?.windowToken, 0)
+    }
+
+
+    private fun calculatePostage() {
         var cCharge = 0
-        var totalCharge =0
+        var totalCharge = 0
         var weightamount = weight.text.toString().toInt()
-        var weightCharge = (weightamount/1000)*750
+        var weightCharge = (weightamount / 1000) * 750
         var gst = 0
 
 
-        if (selectedCountry == "USA"){
+        if (selectedCountry == "USA") {
             cCharge = 50000
-            var grossAmount = cCharge+weightCharge
-            gst = grossAmount*18/100
-            totalCharge = gst+grossAmount
+            var grossAmount = cCharge + weightCharge
+            gst = grossAmount * 18 / 100
+            totalCharge = gst + grossAmount
 
             estimatedCost.text = totalCharge.toString()
-            grossWeight.text = "Gross Weight: "+weight.text+"gms"
-            gstAmount.text = "GST: "+gst.toString()
-            country.text = "Country: $selectedCountry: " +cCharge.toString()
-            estimatedCost.text = "Estimated Cost: "+totalCharge.toString()
+            grossWeight.text = "Gross Weight: " + weight.text + "gms"
+            gstAmount.text = "GST: " + gst.toString()
+            country.text = "Country: $selectedCountry: " + cCharge.toString()
+            estimatedCost.text = "Estimated Cost: " + totalCharge.toString()
 
         }
 
-        if (selectedCountry =="England"){
+        if (selectedCountry == "England") {
             cCharge = 30000
-            var grossAmount = cCharge+weightCharge
-            gst = grossAmount*18/100
-            totalCharge = gst+grossAmount
+            var grossAmount = cCharge + weightCharge
+            gst = grossAmount * 18 / 100
+            totalCharge = gst + grossAmount
 
             estimatedCost.text = totalCharge.toString()
-            grossWeight.text = "Gross Weight: "+weight.text+"gms"
-            gstAmount.text = "GST: "+gst.toString()
-            country.text = "Country: $selectedCountry: " +cCharge.toString()
-            estimatedCost.text = "Estimated Cost: "+totalCharge.toString()
+            grossWeight.text = "Gross Weight: " + weight.text + "gms"
+            gstAmount.text = "GST: " + gst.toString()
+            country.text = "Country: $selectedCountry: " + cCharge.toString()
+            estimatedCost.text = "Estimated Cost: " + totalCharge.toString()
         }
 
-        if (selectedCountry =="France"){
+        if (selectedCountry == "France") {
             cCharge = 45000
-            var grossAmount = cCharge+weightCharge
-            gst = grossAmount*18/100
-            totalCharge = gst+grossAmount
+            var grossAmount = cCharge + weightCharge
+            gst = grossAmount * 18 / 100
+            totalCharge = gst + grossAmount
 
             estimatedCost.text = totalCharge.toString()
-            grossWeight.text = "Gross Weight: "+weight.text+"gms"
-            gstAmount.text = "GST: "+gst.toString()
-            country.text = "Country: $selectedCountry: " +cCharge.toString()
-            estimatedCost.text = "Estimated Cost: "+totalCharge.toString()
+            grossWeight.text = "Gross Weight: " + weight.text + "gms"
+            gstAmount.text = "GST: " + gst.toString()
+            country.text = "Country: $selectedCountry: " + cCharge.toString()
+            estimatedCost.text = "Estimated Cost: " + totalCharge.toString()
         }
 
-        if (selectedCountry =="Russia"){
+        if (selectedCountry == "Russia") {
             cCharge = 20000
-            var grossAmount = cCharge+weightCharge
-            gst = grossAmount*18/100
-            totalCharge = gst+grossAmount
+            var grossAmount = cCharge + weightCharge
+            gst = grossAmount * 18 / 100
+            totalCharge = gst + grossAmount
 
             estimatedCost.text = totalCharge.toString()
-            grossWeight.text = "Gross Weight: "+weight.text+"gms"
-            gstAmount.text = "GST: "+gst.toString()
-            country.text = "Country: $selectedCountry: " +cCharge.toString()
-            estimatedCost.text = "Estimated Cost: "+totalCharge.toString()
+            grossWeight.text = "Gross Weight: " + weight.text + "gms"
+            gstAmount.text = "GST: " + gst.toString()
+            country.text = "Country: $selectedCountry: " + cCharge.toString()
+            estimatedCost.text = "Estimated Cost: " + totalCharge.toString()
         }
 
-        if (selectedCountry =="Japan"){
+        if (selectedCountry == "Japan") {
             cCharge = 25000
-            var grossAmount = cCharge+weightCharge
-            gst = grossAmount*18/100
-            totalCharge = gst+grossAmount
+            var grossAmount = cCharge + weightCharge
+            gst = grossAmount * 18 / 100
+            totalCharge = gst + grossAmount
 
             estimatedCost.text = totalCharge.toString()
-            grossWeight.text = "Gross Weight: "+weight.text+"gms"
-            gstAmount.text = "GST: "+gst.toString()
-            country.text = "Country: $selectedCountry: " +cCharge.toString()
-            estimatedCost.text = "Estimated Cost: "+totalCharge.toString()
+            grossWeight.text = "Gross Weight: " + weight.text + "gms"
+            gstAmount.text = "GST: " + gst.toString()
+            country.text = "Country: $selectedCountry: " + cCharge.toString()
+            estimatedCost.text = "Estimated Cost: " + totalCharge.toString()
         }
 
 
