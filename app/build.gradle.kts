0a5af8b4b72c696060832d4c6501afd43fd27d86
Rajit Deb <rajitdeb456@gmail.com>
2023-10-13 08:41:45 +0530

    Fixed issue (#3) - Missing AddPassword layout file extension and drawable resources of the layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,13 +2,11 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
-
-
 }
 
 android {
     namespace = "com.hindu.dgniryat"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.hindu.dgniryat"
@@ -29,15 +27,18 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
-    }
-    buildFeatures {
-        viewBinding = true
     }
 }
 
--- app/src/main/java/com/hindu/dgniryat/Adapter/ConsignmentAdapter.kt
+++ app/src/main/java/com/hindu/dgniryat/Adapter/ConsignmentAdapter.kt
@@ -12,29 +12,32 @@ import androidx.recyclerview.widget.RecyclerView
 import com.hindu.dgniryat.Model.ArticleModel
 import com.hindu.dgniryat.R
 
-class ConsignmentAdapter(private val mContext: Context,
-                         private val mArticle:List<ArticleModel>):RecyclerView.Adapter<ConsignmentAdapter.ViewHolder>() {
+class ConsignmentAdapter(
+    private val mContext: Context,
+    private val mArticle: List<ArticleModel>
+) : RecyclerView.Adapter<ConsignmentAdapter.ViewHolder>() {
 
-                             inner class ViewHolder(@NonNull itemView: View):RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-                                 private val orderName:TextView= itemView.findViewById(R.id.orderName)
-                                 private val country:TextView = itemView.findViewById(R.id.toCounty_layout)
-                                 private val statusImage: ImageView = itemView.findViewById(R.id.statusImage)
-                                 private val status:TextView = itemView.findViewById(R.id.status)
-                                 private val productType:TextView = itemView.findViewById(R.id.productType_layout)
+        private val orderName: TextView = itemView.findViewById(R.id.orderName)
+        private val country: TextView = itemView.findViewById(R.id.toCounty_layout)
+        private val statusImage: ImageView = itemView.findViewById(R.id.statusImage)
+        private val status: TextView = itemView.findViewById(R.id.status)
+        private val productType: TextView = itemView.findViewById(R.id.productType_layout)
 
 
-                                 fun bind(list:ArticleModel){
-                                     orderName.text = list.orderName
-                                     country.text = list.rCountry
-                                     productType.text = list.productType
-                                     status.text = list.status
-                                 }
+        fun bind(list: ArticleModel) {
+            orderName.text = list.orderName
+            country.text = list.rCountry
+            productType.text = list.productType
+            status.text = list.status
+        }
 
-                             }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(mContext).inflate(R.layout.consignment_item_layout, parent,false)
+        val view =
+            LayoutInflater.from(mContext).inflate(R.layout.consignment_item_layout, parent, false)
         return ViewHolder(view)
     }
 
@@ -47,10 +50,11 @@ class ConsignmentAdapter(private val mContext: Context,
 
         holder.itemView.setOnClickListener {
             val pref = mContext.getSharedPreferences("PREFS", Context.MODE_PRIVATE).edit()
-            pref.putString("orderId",mArticle[position].orderId)
-            pref.putString("articleName",mArticle[position].orderName)
+            pref.putString("orderId", mArticle[position].orderId)
+            pref.putString("articleName", mArticle[position].orderName)
             pref.apply()
-            Navigation.findNavController(holder.itemView).navigate(R.id.action_navigation_dashboard_to_trackingFragment)
+            Navigation.findNavController(holder.itemView)
+                .navigate(R.id.action_navigation_dashboard_to_trackingFragment)
         }
 
     }
